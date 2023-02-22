aef4b02e3135157df418ab21e11148b0068194b1
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-22 16:06:07 +0800

    Add ability to toggle grid size with input range

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,13 @@
+const gridsize = document.querySelector("#grid-size");
+const toggle_value = document.querySelector("#toggle-value");
+const gridboxes = document.querySelectorAll(".grid-boxes");
 const drawingBoard = document.querySelector(".drawing-board");
+
+function setGrid(size) {
+  console.log(size.target.value);
+  removeGrid();
+  createGrid(size.target.value);
+}
 
 function createGrid(size) {
   for (let x = 1; x <= size * size; x++) {
@@ -7,7 +16,16 @@ function createGrid(size) {
     drawingBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     drawingBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     drawingBoard.appendChild(grid_item);
+    toggle_value.textContent = `${size}x${size}`;
   }
 }
 
-createGrid(16);
+function removeGrid() {
+  while (drawingBoard.firstChild) {
+    drawingBoard.removeChild(drawingBoard.lastChild);
+  }
+}
+
+gridsize.addEventListener("change", setGrid);
+
+createGrid(gridsize.value);
