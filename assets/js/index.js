c90efcfd59dc8fc537568843e78ca7b48a28b1e3
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-22 18:20:11 +0800

    Add ability to draw on grid boxes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,10 @@
-const gridsize = document.querySelector("#grid-size");
+// Queries
+const draw_btn = document.querySelector("#draw-button");
 const toggle_value = document.querySelector("#toggle-value");
-const gridboxes = document.querySelectorAll(".grid-boxes");
+const gridsize = document.querySelector("#grid-size");
 const drawingBoard = document.querySelector(".drawing-board");
+
+// Grid functions
 
 function setGrid(size) {
   console.log(size.target.value);
@@ -26,6 +29,30 @@ function removeGrid() {
   }
 }
 
+// Set mouse modes
+
+function setMode() {
+  const grids = document.querySelectorAll(".grid-boxes");
+  for (const box of grids) {
+    box.addEventListener("mouseover", draw);
+  }
+}
+
+// Mouse modes functions
+
+const draw = function (e) {
+  e.target.style["backgroundColor"] = "black";
+};
+
+// Event listeners
+
+draw_btn.addEventListener("click", () => {
+  setMode(1);
+});
+
 gridsize.addEventListener("change", setGrid);
 
+// Default values when page loads and reloads
+
 createGrid(gridsize.value);
+setMode();
