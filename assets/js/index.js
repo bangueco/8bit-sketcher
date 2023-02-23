a5d803be08532be3900c90f388b870ed47ed897e
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-23 13:31:43 +0800

    Make grid boxes not draggable

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,6 +22,7 @@ function createGrid(size) {
   for (let x = 1; x <= size * size; x++) {
     const grid_item = document.createElement("div");
     grid_item.classList.add("grid-boxes");
+    grid_item.setAttribute("draggable", false);
     drawingBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     drawingBoard.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     drawingBoard.appendChild(grid_item);
